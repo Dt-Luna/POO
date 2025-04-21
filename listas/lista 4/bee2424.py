1a0dc22baeaf6687ccfa84c3c49d72cc3127f3e1
Dt-Luna <luna.deodato@escolar.ifrn.edu.br>
2025-04-21 00:23:38 -0300

    outra parte da lista 4

---
+++ listas/lista 4/bee2424.py
@@ -0,0 +1,5 @@
+x, y = int(input()), int(input())
+if (x >= 0 and y>=0) and (x <= 432 and y <= 468):
+    print('dentro')
+else:
+    print('fora')
---
+++ listas/lista 4/bee2670.py
@@ -0,0 +1,3 @@
+a1, a2, a3 = int(input()), int(input()), int(input())
+pisos = [a2 * 2 + a3 * 4 ,a1 * 2 + a3 * 2, a2 * 2 + a1 * 4]
+print(min(pisos))
