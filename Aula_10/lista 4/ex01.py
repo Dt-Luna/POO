e0a6b3987a88117b3587695f091edfbc65dbbf86
Dt-Luna <luna.deodato@escolar.ifrn.edu.br>
2025-06-05 13:25:24 +0000

    a

--- Aula_10/lista 4/ex01.py
+++ Aula_10/lista 4/ex01.py
@@ -1,3 +1,9 @@
 class Viagem:
     def __init__(self):
-        
+    
+    def set_destino(self, s):
+        if len(s)<5: raise ValueError('Valor invalido')
+    def distancia(self, d):
+        if len(s)<5: raise ValueError('Valor invalido')
+    def set_litros(self, s):
+        if len(s)<5: raise ValueError('Valor invalido')
